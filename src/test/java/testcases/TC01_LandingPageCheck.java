e8883c569051deddf3dc4ab38127967fa4b881cd
unknown <pkirupakaran@gmail.com>
2025-03-10 14:05:34 +0530

    Update TC01

--- src/test/java/testcases/TC01_LandingPageCheck.java
+++ src/test/java/testcases/TC01_LandingPageCheck.java
@@ -21,20 +21,10 @@ public class TC01_LandingPageCheck extends WebApplicationWrappers {
 
 	@Test
 	public void landingPageValidation() throws InterruptedException {
-		 Dotenv dotenv = Dotenv.load();
-
-	        // Access the environment variables
-	     String baseUrl = dotenv.get("URL");
 		initDriver("Windows","edge");
-<<<<<<< HEAD
-		Reporter.reportStep("Browser : Edge ","INFO");
-		Reporter.reportStep("Platform : Windows ","INFO");
-		launchApplication(baseUrl);
-=======
 		Reporter.reportStep("Browser : Edge ","USER_INFO");
 		Reporter.reportStep("Platform : Windows ","USER_INFO");
 		launchApplication(loadProp().getProperty("URL"));
->>>>>>> 168e6fec064d4fff4dc56a630b4be6d9624d5c31
 		landingpagenew= new LandingPageNew(driver);
 		
 		landingpagenew.clickgetStartedButton();
